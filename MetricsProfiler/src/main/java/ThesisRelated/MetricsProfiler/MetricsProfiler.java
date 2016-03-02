6e732657d8785daa028ccf243e60d3c30a9e812e
Andrea Muti <muti.andrea@gmail.com>
2016-03-02 18:26:59 +0100

    initial commit

--- MetricsProfiler/src/main/java/ThesisRelated/MetricsProfiler/MetricsProfiler.java
+++ MetricsProfiler/src/main/java/ThesisRelated/MetricsProfiler/MetricsProfiler.java
@@ -10,6 +10,17 @@ package ThesisRelated.MetricsProfiler;
 public class MetricsProfiler {
     public static void main( String[] args ){
         System.out.println("This is the MetricsProfiler");
-        System.out.println("seconda print");
+        
+        int input_rate;				// preso come parametro
+        int num_nodes;				// leggo dal java_driver
+        long cpu_level; 			// leggo da cassandra jmx
+        int throughput_total;		// calcolo leggendo da cassandra jmx
+        double rt_mean;				// leggo da java_driver
+        double rt_95p;				// leggo da java_driver
+        
+        
+        
+        
+        
     }
 }
